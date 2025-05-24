573158c39550a5c66d260af433c6d4ac1736f374
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2025-05-24 11:21:43 +0530

    Dot loader added

---
+++ src/app/components/dot-loader/dot-loader.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dot-loader',
+  templateUrl: './dot-loader.component.html',
+  styleUrls: ['./dot-loader.component.scss']
+})
+export class DotLoaderComponent { }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
+import { RegistrationFormComponent } from './app/components/registration-form/registration-form.component';
+import { DotLoaderComponent } from './app/components/dot-loader/dot-loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RegistrationFormComponent],
+  imports: [CommonModule, RegistrationFormComponent, DotLoaderComponent],
   template: `
-    <div class="container">
-      <h1 class="text-center">Angular Forms Validation Example</h1>
-      <p class="text-center">This example demonstrates how to use custom validators with reactive forms in Angular</p>
-      
-      <app-registration-form></app-registration-form>
-    </div>
+    @if(showLoader) {
+      <app-dot-loader class="loader-container"></app-dot-loader>
+    } @else {
+      <div class="container">
+        <h1 class="text-center">Angular Forms Validation Example</h1>
+        <p class="text-center">This example demonstrates how to use custom validators with reactive forms in Angular</p>
+        
+        <app-registration-form></app-registration-form>
+      </div>
+    }
   `,
   styles: [
     `
@@ -37,11 +42,25 @@ import { RegistrationFormComponent } from './components/registration-form/regist
     .text-center {
       text-align:center !important;
     }
+
+    .loader-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      width: 100vw;
+    }
   `,
   ],
 })
 export class App {
   title = 'Angular Forms Validation';
+  showLoader = true;
+  constructor() {
+    setTimeout(() => {
+      this.showLoader = false;
+    }, 2000);
+  }
 }
 
 bootstrapApplication(App, {
