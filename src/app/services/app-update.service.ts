f74f3fc71f4f2e90fe51e8ce65095820b732e063
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2025-05-24 13:45:30 +0530

    Issues fixed

--- src/app/services/app-update.service.ts
+++ src/app/services/app-update.service.ts
@@ -1,24 +1,47 @@
-import { Injectable } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { SwUpdate, VersionReadyEvent } from "@angular/service-worker";
+import { tap, shareReplay, interval, startWith, filter } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AppUpdateService {
-  constructor(private swUpdate: SwUpdate) {
-    if (this.swUpdate.isEnabled) {
-      this.swUpdate.versionUpdates.subscribe(evt => {
-        if (evt.type === 'VERSION_READY') {
-          // Simple confirm dialog for update
-          const updateConfirmed = window.confirm(
-            'A new version of the app is available. Would you like to update now?'
-          );
+  readonly available$ = this.updates.versionUpdates.pipe(
+    filter((evt): evt is VersionReadyEvent => evt.type === "VERSION_READY"),
+    tap((event: VersionReadyEvent) => {
+      console.info(
+        `App Update available: ${event.latestVersion.hash}. Current version: ${event.currentVersion.hash}.`
+      );
+    }),
+    shareReplay(1)
+  );
 
-          if (updateConfirmed) {
-            this.swUpdate.activateUpdate().then(() => document.location.reload());
+  constructor(private updates: SwUpdate, private router: Router) {
+    console.log("App Update constructor");
+    if ("serviceWorker" in navigator) {
+      console.log("App Update serviceWorker is listening");
+      interval(60 * 1000)
+        .pipe(startWith(0))
+        .subscribe(async () => {
+          try {
+            const res = await this.updates.checkForUpdate();
+            console.log("App Update check result:", res);
+          } catch (error) {
+            console.error(error);
+            console.log("App Update check failed:", error);
           }
-        }
-      });
+        });
+    }
+  }
+
+  async activateUpdate(): Promise<void> {
+    try {
+      await this.updates.activateUpdate();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.router.navigate(["/"], { replaceUrl: true });
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,52 @@
 import { CommonModule } from "@angular/common";
-import { Component, isDevMode } from "@angular/core";
+import { Component, isDevMode, OnDestroy } from "@angular/core";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { RegistrationFormComponent } from "./app/components/registration-form/registration-form.component";
 import { DotLoaderComponent } from "./app/components/dot-loader/dot-loader.component";
 import { provideServiceWorker } from "@angular/service-worker";
 import { AppUpdateService } from "./app/services/app-update.service";
+import { takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
   selector: "app-root",
   standalone: true,
   imports: [CommonModule, RegistrationFormComponent, DotLoaderComponent],
   template: `
+      <div
+      *ngIf="(_appUpdateService.available$ | async) || true"
+      class="msg-wrapper"
+    >
+      A new version of this site is available.
+      <a (click)="_appUpdateService.activateUpdate()" class="reload-link">Reload Now</a> to
+      activate it.
+    </div>
+
+
     @if(showLoader) {
     <app-dot-loader class="loader-container"></app-dot-loader>
     } @else {
     <app-registration-form class="container"></app-registration-form>
-
     }
   `,
 })
-export class App {
+export class App implements OnDestroy{
   showLoader = true;
-  constructor(private readonly _appUpdateService: AppUpdateService) {
+  onDestroy = new Subject<void>();
+
+  constructor(readonly _appUpdateService: AppUpdateService) {
     setTimeout(() => {
       this.showLoader = false;
     }, 2000);
+
+      this._appUpdateService.available$
+      .pipe(takeUntil(this.onDestroy))
+      .subscribe((res: any) => { console.log('App Update available? -> ', res); });
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy.next();
+    this.onDestroy.complete();
   }
 }
 
