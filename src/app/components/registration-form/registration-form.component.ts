d900388c4b2b020a3eefba1e5e8d9cb496d7f2c6
niteshprakashkesarkar3 <niteshprakashkesarkar3@gmail.com>
2025-05-24 16:48:30 +0530

    Updated registration-form.component.ts

--- src/app/components/registration-form/registration-form.component.ts
+++ src/app/components/registration-form/registration-form.component.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { CustomValidators } from '../../validators/validators.util';
-import { WellnessScoreDialogComponent } from 'src/app/components/wellness-score-dialog/wellness-score-dialog.component.ts';
+import { WellnessScoreDialogComponent } from '../wellness-score-dialog/wellness-score-dialog.component.ts';
 
 @Component({
   selector: 'app-registration-form',
